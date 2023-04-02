30e6238e1a458805fd8f2dc0b741538c65e5c699
shrikant2753 <shrikantrp.00@gmail.com>
2023-04-02 18:50:04 +0530

    solution

--- src/main/java/com/driver/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/driver/services/impl/CustomerServiceImpl.java
@@ -61,7 +61,7 @@ public class CustomerServiceImpl implements CustomerService {
 			}
 		}
 		if(driver==null){
-			throw  new Exception("No cab available");
+			throw  new Exception("No cab available!");
 		}
 
 		Customer customer = customerRepository2.findById(customerId).get();
