2733e7d25a431210074a557f795bb354505d4bad
shrikant2753 <shrikantrp.00@gmail.com>
2023-04-02 18:35:12 +0530

    solution

--- src/main/java/com/driver/model/Cab.java
+++ src/main/java/com/driver/model/Cab.java
@@ -37,6 +37,10 @@ public class Cab {
         return available;
     }
 
+    public boolean getAvailable() {
+        return available;
+    }
+
     public void setAvailable(boolean available) {
         this.available = available;
     }
